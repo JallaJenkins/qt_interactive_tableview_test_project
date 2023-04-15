dd6572d7a9cf8f8898074973f88d81b4c7219057
JallaJenkins <wakeupalready@gmail.com>
2023-04-15 13:07:21 -0700

    deleted test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import (
-    QWidget,
-    QItemDelegate,
-    QStyledItemDelegate,
-    QStyleOptionViewItem,
-    QStyleOptionButton,
-)
-
-print(QStyleOptionButton.__dict__)
